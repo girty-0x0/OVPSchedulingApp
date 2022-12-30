68e60c2ecf9b9369ae005a6bc797ae790ed9f470
girty-0x0 <girty.0x0@protonmail.com>
2022-12-29 17:20:53 -0700

    Added database connection

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,7 @@
 package sample;
 
+import helper.JDBC;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,6 +20,8 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        JDBC.openConnection();
         launch(args);
+        JDBC.closeConnection(); //closed after app is closed out
     }
 }
